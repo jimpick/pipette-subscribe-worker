ecbf575fa277a9a83b2c8dedfc68385893318079
Jim Pick <jim@backblend.com>
2017-10-22 09:56:31 -0700

    More debugging tools

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Dat = require('dat-node')
 const datDns = require('dat-dns')()
 const minimist = require('minimist')
 const mkdirp = require('mkdirp')
+const { throttle } = require('lodash')
 
 const argv = minimist(process.argv.slice(2), {
   alias: {
@@ -40,7 +41,29 @@ function subscribeToDat (key) {
       if (error) {
         return reject(error)
       }
-      dat.joinNetwork()
+      dat.joinNetwork(error => {
+        if (error) {
+          console.error('joinNetwork error', error)
+          throw err
+        }
+        console.log('Network joined')
+        const { network } = dat
+        const { connected, connecting, queued } = network
+        console.log('Network:', connected, connecting, queued)
+
+        // After the first round of network checks, the callback is called
+        // If no one is online, you can exit and let the user know.
+        /*
+        setTimeout(() => {
+          const { connected, connecting, queued } = network
+          console.log('Network:', connected, connecting, queued)
+          if (!connected && !connecting && !queued) {
+            console.error('No users currently online for that key.')
+            process.exit(1)
+          }
+        }, 10000)
+        */
+      })
       resolve(dat)
     })
   })
@@ -69,15 +92,42 @@ function networkTools (dat) {
 
 function datStatus (dat) {
   const stats = dat.trackStats()
-  stats.on('update', () => {
+  stats.on('update', throttle(() => {
     const st = stats.get()
     console.log('Stats update:', st)
-  })
+  }, 1000))
   setInterval(() => {
     console.log('Stats network:', stats.network.downloadSpeed,
       stats.network.uploadSpeed)
     console.log('Stats peers:', stats.peers.total, stats.peers.complete)
   }, 5000)
+}
+
+function archiveTools (archive) {
+  archive.on('ready', () => {
+    console.log('Archive ready', archive.version)
+  })
+  archive.on('update', () => {
+    console.log('Archive update', archive.version)
+  })
+  archive.on('content', () => {
+    console.log('Archive content')
+  })
+  archive.on('syncing', () => {
+    console.log('Archive syncing')
+  })
+  archive.on('sync', () => {
+    console.log('Archive sync')
+  })
+  archive.on('appending', () => {
+    console.log('Archive appending')
+  })
+  archive.on('append', () => {
+    console.log('Archive append')
+  })
+  archive.on('error', error => {
+    console.log('Archive error', error)
+  })
 }
 
 async function run ({ sourceDatUrl, subscribe, share }) {
@@ -87,6 +137,8 @@ async function run ({ sourceDatUrl, subscribe, share }) {
   const dat = await subscribeToDat(key)
   // networkTools(dat)
   datStatus(dat)
+  const { archive } = dat
+  archiveTools(archive)
   // console.log('Jim', dat)
 }
 
